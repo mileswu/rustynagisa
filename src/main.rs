0fd32e0b96dd7a75228f37ced146f627e561d804
Miles Wu <miles.wu@wu-home.co.uk>
2016-04-01 00:42:32 -0500

    style tidyup

--- src/main.rs
+++ src/main.rs
@@ -25,18 +25,19 @@ fn main() {
             let (_, arguments) = text.split_at(command.len());
             let _result = match command {
                 "!w" => weather(&server, channel, arguments.trim()),
-                _ => Ok(())
+                _ => Ok(()),
             };
         }
     }
-    return ();
+    return;
 }
 
 fn weather(server: &IrcServer, channel: &str, arguments: &str) -> Result<(), ()> {
     let httpclient = hyper::Client::new();
-    let apikey = match server.config().options.as_ref().and_then(|ref i| i.get("weather_apikey")) {
-        Some(j) => j,
-        None => return Err(())
+    let apikey = match server.config().options.as_ref()
+        .and_then(|i| i.get("weather_apikey")) {
+            Some(j) => j,
+            None => return Err(()),
     };
     let httpurl = format!("http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}", percent_encoding::percent_encode(arguments.as_bytes(), percent_encoding::QUERY_ENCODE_SET), apikey);
     let mut httpreq = match httpclient.get(&httpurl)
@@ -50,17 +51,19 @@ fn weather(server: &IrcServer, channel: &str, arguments: &str) -> Result<(), ()>
 
     let data = match Json::from_str(&httpbody) {
         Ok(d) => d,
-        Err(_) => return Err(())
+        Err(_) => return Err(()),
     };
     println!("{}", data);
 
     let returncode = match data.find("cod") {
         Some(i) => i,
-        None => return Err(())
+        None => return Err(()),
     };
     if returncode.is_string() {
         if returncode.as_string().unwrap() == "404" {
-            server.send_privmsg(channel, &format!("Location ({}) not found", arguments)).unwrap();
+            server.send_privmsg(channel,
+                                &format!("Location ({}) not found", arguments))
+                .unwrap();
         }
         return Err(());
     }
@@ -74,44 +77,62 @@ fn weather(server: &IrcServer, channel: &str, arguments: &str) -> Result<(), ()>
         return Err(());
     }
 
-    let city = match data.find("name").and_then(|i| i.as_string()) {
-        Some(j) => j,
-        None => return Err(())
+    let city = match data.find("name")
+        .and_then(|i| i.as_string()) {
+            Some(j) => j,
+            None => return Err(()),
     };
-    let country = match data.find("sys").and_then(|i| i.find("country")).and_then(|j| j.as_string()) {
-        Some(k) => k,
-        None => return Err(())
+    let country = match data.find("sys")
+        .and_then(|i| i.find("country"))
+        .and_then(|j| j.as_string()) {
+            Some(k) => k,
+            None => return Err(()),
     };
 
-    let weather_arr = match data.find("weather").and_then(|i| i.as_array()) {
-        Some(j) => j,
-        None => return Err(())
+    let weather_arr = match data.find("weather")
+        .and_then(|i| i.as_array()) {
+            Some(j) => j,
+            None => return Err(()),
     };
     if weather_arr.len() == 0 {
         return Err(());
     }
-    let weather_main = match weather_arr[0].find("main").and_then(|i| i.as_string()) {
-        Some(j) => j,
-        None => return Err(())
+    let weather_main = match weather_arr[0].find("main")
+        .and_then(|i| i.as_string()) {
+            Some(j) => j,
+            None => return Err(()),
     };
-    let weather_desc = match weather_arr[0].find("description").and_then(|i| i.as_string()) {
-        Some(j) => j,
-        None => return Err(())
+    let weather_desc = match weather_arr[0].find("description")
+        .and_then(|i| i.as_string()) {
+            Some(j) => j,
+            None => return Err(()),
     };
-    let weather_temp = match data.find("main").and_then(|i| i.find("temp")).and_then(|j| j.as_f64()) {
+    let weather_temp = match data.find("main")
+        .and_then(|i| i.find("temp"))
+        .and_then(|j| j.as_f64()) {
+            Some(k) => k,
+            None => return Err(()),
+    };
+    let weather_humidity = match data.find("main")
+        .and_then(|i| i.find("humidity")).and_then(|j| j.as_f64()) {
         Some(k) => k,
-        None => return Err(())
+        None => return Err(()),
     };
-    let weather_humidity = match data.find("main").and_then(|i| i.find("humidity")).and_then(|j| j.as_f64()) {
-        Some(k) => k,
-        None => return Err(())
-    };
-    let weather_windspeed = match data.find("wind").and_then(|i| i.find("speed")).and_then(|j| j.as_f64()) {
-        Some(k) => k,
-        None => return Err(())
+    let weather_windspeed = match data.find("wind")
+        .and_then(|i| i.find("speed"))
+        .and_then(|j| j.as_f64()) {
+            Some(k) => k,
+            None => return Err(()),
     };
 
-    let reply = format!("{}, {}: {} ({}) {}C, {} m/s wind, {}% humidity", city, country, weather_main, weather_desc, weather_temp, weather_windspeed, weather_humidity);
+    let reply = format!("{}, {}: {} ({}) {}C, {} m/s wind, {}% humidity",
+        city,
+        country,
+        weather_main,
+        weather_desc,
+        weather_temp,
+        weather_windspeed,
+        weather_humidity);
 
     server.send_privmsg(channel, &reply).unwrap();
     return Ok(());
